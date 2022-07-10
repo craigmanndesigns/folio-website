c808f5fe49e255fc39cd4f49a551e91297d5c6d9
Craig Mann <craig.mann@rgvqcfl7ch.lan>
2022-07-10 14:22:51 +0100

    scroll animation

--- src/components/home_bg.js
+++ src/components/home_bg.js
@@ -2,14 +2,14 @@ import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import Window from '../images/window.svg'
+import Window from '../images/window_1.svg'
 
 const HomeBg = ({  }) => {
   gsap.registerPlugin(ScrollTrigger);
 
   let sections = gsap.utils.toArray(".main-sections");
   
-
+  
   useEffect(() => {
     gsap.to(sections, {
       xPercent: -100 * (sections.length - 1),
@@ -19,28 +19,32 @@ const HomeBg = ({  }) => {
         pin: true,
         scrub: 1,
         snap: 1 / (sections.length - 1),
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        // markers: true,
         end: "+=3500",
       }
     });
-
-    gsap.timeline({defaults: {duration: 1},
+    gsap.set("#clippath", {transformOrigin: 'top left', scaleY: 0, scaleX: 0.16, rotate: '66deg'})
+    let t = gsap.timeline({
       scrollTrigger: {
-        trigger: "#intro_id",
+        trigger: "#light",
         scrub: true,
         start: "top center",
-        end: "bottom center",
+        end: "top 35%",
         markers: true,
-      }})
+      }
+    })
+    // t 
+    t.to("#clippath",{ scaleY: .7, duration: 0.5})
+    t.to("#clippath",{ scaleY: 1, scaleX: 1, rotate: '0deg', duration: 1}, ">")
+    t.to("#light", { opacity: 1, duration: 3}, "<");
+
     
     
   }, []);
   
   return (
-    <>
-    <Window />
-    </>
+    <div className="bg_anim home">
+      <Window />
+    </div>
   );
 };
  
